390cf2c9218d21ce75fdf777e4596a5e17c654a6
Nguyễn Quốc Khánh <nqk25092002@gmail.com>
2024-06-07 14:10:42 +0700

    1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Categories\CategoriesController;
+use App\Http\Controllers\Api\User\LoginFacebookController;
+use App\Http\Controllers\Api\User\LoginGoogleController;
 use App\Http\Controllers\Api\User\UserController;
 use App\Http\Controllers\Payment\PaymentController;
 use Illuminate\Http\Request;
@@ -32,6 +35,8 @@ Route::middleware('auth:api')->group(function () {
     });
 });
 
+
+// CRUD user
 Route::group(['prefix' => 'users'], function () {
     Route::post('create', [UserController::class, 'store']);
     Route::get('index', [UserController::class, 'index']);
@@ -39,4 +44,21 @@ Route::group(['prefix' => 'users'], function () {
     Route::get('check-login', [UserController::class, 'checkLogin'])->name('checklLogin');
     Route::get('check-permission-fail-store', [UserController::class, 'checkPermissionFailStore'])->name('checkPermissionFailStore');
     Route::post('update/{id}', [UserController::class, 'update']);
+});
+
+// Google Sign In
+Route::get('google-sign-in-url', [LoginGoogleController::class, 'googleSignInUrl']);
+Route::get('google/callback', [LoginGoogleController::class, 'loginGoogleCallback']);
+
+// Login by facebook
+Route::get('facebook-sign-in-url', [LoginFacebookController::class, 'facebookSignInUrl']);
+Route::get('facebook/callback', [LoginFacebookController::class, 'loginFacebookCallback']);
+
+
+Route::group(['prefix' => 'categories'], function () {
+    Route::post('create', [CategoriesController::class, 'store']);
+    Route::get('index', [CategoriesController::class, 'index']);
+    Route::get('show/{id}', [CategoriesController::class, 'showCategories']);
+    Route::post('update/{id}', [CategoriesController::class, 'update']);
+
 });
